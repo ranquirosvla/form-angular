c7c9443619c110771ec4aa6d610f2c32ca14bfbc
ranquirosvla <rsqrodriguez@vlacademy.com>
2025-05-28 10:39:22 -0600

    Se crea el FormGroup en component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, Validators, FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
 export class AppComponent {
-  title = 'form-angular';
+  //title = 'form-angular';
+  productForm = new FormGroup({
+    title: new FormControl('', Validators.required),    
+    price: new FormControl(Validators.required, Validators.pattern('^[0-9]+$')),
+    description: new FormControl('', Validators.required),
+    category: new FormControl (Validators.required, Validators.pattern('^[0-9]+$')),  
+  });   
+  onSubmit() {
+    if (this.productForm.valid) {
+      console.log('Form Submitted!', this.productForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  } 
 }
